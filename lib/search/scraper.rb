5d18e8badf215e5b862387a5087e3eea656b9713
edmundopierre@yahoo.com <edmundo@LAPTOP-LTB84QPJ.localdomain>
2020-04-14 03:49:02 -0700

    make an instance method make_jobs

--- lib/search/scraper.rb
+++ lib/search/scraper.rb
@@ -2,10 +2,23 @@ class Scraper
 
     def self.get_job_name(job_name_)
            
-           site = "https://www.indeed.com/jobs?q=#{job_name_}"
-          doc = Nokogiri::HTML(open(site))  
+          site = "https://www.indeed.com/jobs?q=#{job_name_}"
 
-          section = doc.css("a.jobtitle")
+          doc = Nokogiri::HTML(open(site)) 
+    
+    def scrape_page      
+          doc.css("a.jobtitle")
+
+    end
+
+ 
+    def make_jobs 
+
+        scrape_page.each do |job_value|
+            Module::Jobs.job_index(job_value)
+        end
+
+
           binding.pry
     end
 end
